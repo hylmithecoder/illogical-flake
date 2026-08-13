a64e84b291eda9c592b38f3f6bf56919db062f96
hylmithecoder <hylmimuhammadfiarymahdi@gmail.com>
2026-08-13 11:56:38 +0700

    remove: gnome-icon-theme

--- home-modules/packages.nix
+++ home-modules/packages.nix
@@ -115,7 +115,7 @@ in
       papirus-icon-theme  # Primary icon theme
       adwaita-icon-theme  # GNOME fallback icons
       hicolor-icon-theme  # Base icon theme (required by most themes)
-      gnome-icon-theme  # Additional GNOME icon coverage
+      #gnome-icon-theme Additional GNOME icon coverage
       kdePackages.breeze-icons  # KDE Breeze icons (required by Papirus inheritance)
 
       # Cloudflare-warp
